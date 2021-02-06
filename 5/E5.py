6c4b211899e5f8f3dcb73adee94ad258613ea36f
Emil Rapp <emilemail4@gmail.com>
2021-02-06 18:09:08 +0100

    Update E5.py

--- 5/E5.py
+++ 5/E5.py
@@ -1,9 +1,3 @@
-"""
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-"""
-
-
 def is_divisible_by(number, max_divisor):
     for i in range(1, max_divisor + 1):
         if number % i != 0:
